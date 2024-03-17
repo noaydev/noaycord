d255e6aff82e08a66e1e82cc60bebab165f30b7a
noaydev <noaydev@gmail.com>
2024-03-17 15:58:31 +0200

    Added message and user classes for future use

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from .classes import Bot
+from .classes import *
 from .intents import Intents, get_all_intents
--- classes.py
+++ classes.py
@@ -21,3 +21,17 @@ class Bot:
             asyncio.create_task(self.event_handlers['ready']())
             return
         return wrapper
+
+class User:
+    def __init__(self, id: int, username: str, display_name: str, discriminator: str) -> None:
+        self.id = id
+        self.username = username
+        self.display_name = display_name
+        self.discriminator = discriminator
+
+class Message:
+    def __init__(self, id: int, channel_id: int, author: User, content: str) -> None:
+        self.id = id
+        self.channel_id = channel_id
+        self.author = author
+        self.content = content
